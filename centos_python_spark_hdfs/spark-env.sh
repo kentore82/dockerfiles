c7b4ddf256b4a638baf411c6037d584786668275
kentt <tallakstad@gmail.com>
2020-10-14 13:19:05 +0200

    Fixed spark conf

--- centos_python_spark_hdfs/spark-env.sh
+++ centos_python_spark_hdfs/spark-env.sh
@@ -6,7 +6,7 @@ export PYSPARK_PYTHON="/usr/bin/python3"
 # for driver, defaults to PYSPARK_PYTHON
 export PYSPARK_DRIVER_PYTHON="/usr/bin/python3"
 
-export HADOOP_HOME="/user/local/hadoop"
+export HADOOP_HOME="/usr/local/hadoop"
 
 HADOOP_CONF_DIR=${HADOOP_HOME}/etc/hadoop
 export HADOOP_CONF_DIR
